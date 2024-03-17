cc3711435368e6e1cf0697ae9d3925e98ed562b8
szymo <szymon.pieta2@o2.pl>
2024-03-17 12:24:43 +0100

    test

--- lab1.py
+++ lab1.py
@@ -1,3 +1,24 @@
-import
-test1
-dsad
+import numpy as np
+from tkinter import filedialog
+import tkinter as tk
+
+def generate_and_save_points_flat_horizontal(width, length, num_points, filepath):
+    x_coords = np.random.uniform(0, width, num_points)
+    y_coords = np.random.uniform(0, length, num_points)
+    z_coords = np.zeros(num_points)
+    points = np.column_stack((x_coords, y_coords, z_coords))
+    np.savetxt(filepath, points, fmt='%.6f', header='x y z', comments='')
+    root = tk.Tk()
+    root.withdraw()
+    save_path = filedialog.asksaveasfilename(defaultextension=".xyz",filetypes=(("XYZ files","*.xyz"),("ALL files","*.*")),initialfile=f'horizontal_{num_points}.xyz')
+    if save_path:
+        np.savetxt(save_path, points, delimiter=' ', fmt='%.6f')
+        print("Point cloud saved successfully.")
+    else:
+        print("Save operation canceled.")
+# Przykład użycia
+width = 10
+length = 20
+num_points = 100
+filepath = 'flat_horizontal_points.xyz'
+generate_and_save_points_flat_horizontal(width, length, num_points, filepath)
