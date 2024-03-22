90587089f6e0eda70dab23164e7fa6d82e50886f
szymo <szymon.pieta2@o2.pl>
2024-03-22 21:32:28 +0100

    test

---
+++ lab1apoi.py
@@ -0,0 +1,34 @@
+from scipy.stats import norm
+from csv import writer
+from tkinter import Tk, filedialog
+
+
+def generate_points(num_points: int = 2000):
+    distribution_x = norm(loc=0, scale=20)
+    distribution_y = norm(loc=0, scale=20)
+    #distribution_z = norm(loc=0, scale=20)
+
+    x = distribution_x.rvs(size=num_points)
+    y = distribution_y.rvs(size=num_points)
+    #z = distribution_z.rvs(size=num_points)
+
+    points = zip(x, y,)
+    return points
+
+
+if __name__ == '__main__':
+    num_points = 2000
+    cloud_points = generate_points(num_points)
+
+    root = Tk()
+    root.withdraw()  # Hide the main window
+
+    file_path = filedialog.asksaveasfilename(defaultextension=".xyz")
+    if file_path:
+        with open(file_path, 'w', encoding='utf-8', newline='\n') as csvfile:
+            csvwriter = writer(csvfile)
+            for p in cloud_points:
+                csvwriter.writerow(p)
+        print("Dane zostały zapisane.")
+    else:
+        print("Operacja zapisu została anulowana.")
